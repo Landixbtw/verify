5ca83192a538d01e10fdbfee2e09e3ffbb973406
Landixbtw <68870626+Landixbtw@users.noreply.github.com>
2025-07-25 00:54:19 +0200

    s

--- src/cogs/email_verification/email_service.py
+++ src/cogs/email_verification/email_service.py
@@ -6,11 +6,21 @@ class EmailService:
     @staticmethod
     def send_verification_email(email: str, code: str, username: str) -> None:
         msg = MIMEText(
+            "(english below)"
             f"Hallo {username},\n\n"
             f"Dein Verifizierungscode lautet: {code}\n\n"
             f"Bitte gib diesen Code mit dem Befehl {Config.PREFIX}confirm <code> ein.\n"
             "Der Code läuft in 5 Minuten ab.\n\n"
             "Mit freundlichen Grüßen,\nDein Verifikations-Bot"
+            "\n"
+            "\n"
+            "\n"
+            f"Hello {username},\n\n"
+            f"Your Verifikation Code is {code}\n\n"
+            f"Please enter your code with the command {Config.PREFIX}confirm <code>.\n"
+            "This code expires in 5minutes.\n\n"
+            "Kind regards, your verification bot"
+
         )
         
         msg['Subject'] = 'Discord Verifikationscode'
